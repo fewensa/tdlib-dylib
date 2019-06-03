1534093b5a8437d2f39cd02190e9fa673def93e5
fewensa <fewensa@protonmail.com>
2019-06-03 08:58:32 +0800

    test

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@
 
 BIN_PATH=$(dirname $(readlink -f $0))
 BUILD_PATH=/tmp/tdlib/td
-SOURCE_PATH=/tmp/tdlib/source
 ARCH=$(arch)
 
 
@@ -23,33 +22,19 @@ build() {
   cd $BUILD_PATH
   git checkout -b $VERSION origin/$VERSION
 
-  docker run --rm -dit --name alpine -v $BUILD_PATH:/source -v $BIN_PATH/tdbuild.sh:/tdbuild.sh alpine:3.9
+  docker run --rm -dit --name alpine \
+    -v $BUILD_PATH:/source \
+    -v $BIN_PATH/tdbuild.sh:/tdbuild.sh \
+    -e TG_TOKEN=$GH_TOKEN \
+    -e VERSION=$VERSION \
+    alpine:3.9
   docker exec -it alpine sh -f /tdbuild.sh
   docker stop alpine
-  sudo chown $USER:$USER -R $BUILD_PATH
-
-  move_dylib $VERSION
 
   cd $BIN_PATH
   rm -rf $BUILD_PATH
 }
 
-move_dylib() {
-  VERSION=$1
-
-  TARGET_PATH=$SOURCE_PATH/linux/$VERSION/$ARCH
-  mkdir -p $TARGET_PATH
-  mv $BUILD_PATH/build/libtdjson.so* $TARGET_PATH
-
-  cd $SOURCE_PATH
-  git add .
-  git commit -m "$VERSION - $ARCH"
-  git push origin master
-
-  ls -la $TARGET_PATH
-
-  cd $BIN_PATH
-}
 
 main() {
   ALL_VERSION=$(echo -e $(curl https://github.com/tdlib/td/releases | pup '.release-entry .commit-title a text{}'))
@@ -58,11 +43,11 @@ main() {
   git config --local user.email fewensa@protonmail.com
   git config --local user.name fewensa
 
-  for V in $ALL_VERSION
-  do
-    build $V
-  done
-  # build v1.4.0
+  # for V in $ALL_VERSION
+  # do
+  #   build $V
+  # done
+  build v1.4.0
 
 
 }
--- tdbuild.sh
+++ tdbuild.sh
@@ -5,13 +5,36 @@
 
 
 SOURCE=/source
+BUILD_PATH=$SOURCE/build
+DYLIB_PATH=/tmp/tdlib-dylib
+ARCH=$(arch)
+
 
 apk update
 apk add gcc g++ cmake make zlib-dev openssl-dev linux-headers gperf git
 
 
-mkdir -p $SOURCE/build
-cd $SOURCE/build
+mkdir -p $BUILD_PATH
+cd $BUILD_PATH
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
+
+git clone https://github.com/fewensa/tdlib-dylib.git $DYLIB_PATH
+
+echo 'https://$GH_TOKEN@github.com' > ~/.git-credentials
+git config --global credential.helper store
+
+
+TARGET_PATH=$DYLIB_PATH/linux/$VERSION/$ARCH
+mkdir -p $TARGET_PATH
+ls $BUILD_PATH -l
+mv $BUILD_PATH/libtdjson.so* $TARGET_PATH
+ls -la $TARGET_PATH
+
+cd $DYLIB_PATH
+
+git add .
+git commit -m "$VERSION - $ARCH"
+git push origin master
+
 
